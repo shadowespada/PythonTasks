9cd56388b87b9c20150003b86ec2103e0ba53f1a
shadowespada <shadowespada@users.noreply.github.com>
2020-01-25 13:07:34 +0200

    Age Python Task

---
+++ task2.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+
+def Age(birth_year):
+    now = datetime.now().year
+    print('Current year : ', now)
+    current_age = now - birth_year
+    print(f'Your age is {current_age}')
+
+
+birth_year = int(input('Enter your birth year: '))
+Age(birth_year)
